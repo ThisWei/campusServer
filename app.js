921b6f7880419bb76ea4a9343cfaf5f426ca4ee1
thiswei <1255990618@qq.com>
2022-12-02 16:34:05 +0800

    家中

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.set('view engine', 'jade');
 
 //全局中间件
 app.use(function (req, res, next) {
-  console.log('中');
+  console.log('中的');
   res.ss = function (code, message, data) {
     let content = { code, message }
     data ? content.data = data : ''
